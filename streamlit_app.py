d14b0ad2243049739997d580f7c7474a94e8081e
Luca Braglia <lbraglia@gmail.com>
2025-10-16 16:03:03 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,33 @@
-import jinja2
-import tempfile
-import streamlit as st
-
-# File uploader
-xlsx = st.file_uploader(label = "Fai l'upload del file struttura qui, dopodiché clicca su 'Download CRF'.",
-                        type = ["xlsx"],
-                        accept_multiple_files = False)
-
-if xlsx is not None:
-    struc = tempfile.NamedTemporaryFile(suffix = '.xlsx')
-    out = tempfile.NamedTemporaryFile(suffix = '.xlsx')
-    strucfile = struc.name
-    outfile = out.name
-    # salvo per comodità il file in un file
-    with open(strucfile, "wb") as f:
-        f.write(xlsx.getbuffer())
-    crf = CRF()
-    crf.read_structure(strucfile)
-    crf.create(outfile)
-    with open(outfile, "rb") as f:
-        btn = st.download_button(
-            label = "Download CRF",
-            data = f,
-            file_name = "crf.xlsx")
-    
-# Include README
-with open('frontpage.md', 'r') as fp:
-    frontpage = fp.read()
-st.markdown(frontpage)
-        
-
-
 # https://realpython.com/primer-on-jinja-templating/
-from pathlib import Path
 import datetime as dt
+import jinja2
+import streamlit as st
 import subprocess
 import tempfile
 
-path = Path("jinja_templates") / "spirit2025.md"
-with open(path, "r") as f:
+from pathlib import Path
+
+
+# # File uploader
+# xlsx = st.file_uploader(label = "Fai l'upload del file struttura qui, dopodiché clicca su 'Download CRF'.",
+#                         type = ["xlsx"],
+#                         accept_multiple_files = False)
+
+# if xlsx is not None:
+#     struc = tempfile.NamedTemporaryFile(suffix = '.xlsx')
+#     out = tempfile.NamedTemporaryFile(suffix = '.xlsx')
+#     strucfile = struc.name
+#     outfile = out.name
+#     # salvo per comodità il file in un file
+#     with open(strucfile, "wb") as f:
+#         f.write(xlsx.getbuffer())
+#     crf = CRF()
+#     crf.read_structure(strucfile)
+#     crf.create(outfile)
+    
+
+template_path = Path("jinja_templates") / "spirit2025.md"
+with open(template_path, "r") as f:
     content = f.read()
 
 
@@ -59,5 +48,20 @@ with open(tmpfname, "w") as f:
     print(f"file {tmpfname} created")
 
 
+# pandoc
 outfile = Path(f"/tmp/{variables["ACRONIMO_STUDIO"]}_study_protocol.docx")
-subprocess.run(["pandoc", tmpfname, "-f", "gfm", "-t", "docx", "-o", str(outfile)])
+pandoc = ["pandoc", tmpfname, "-f", "gfm", "-t", "docx", "-o", str(outfile)]
+subprocess.run(pandoc)
+
+
+with open(outfile, "rb") as f:
+    btn = st.download_button(
+        label="Download Protocol",
+        data=f,
+        file_name="protocol_template.docx")
+
+
+# Include README
+with open('frontpage.md', 'r') as fp:
+    frontpage = fp.read()
+st.markdown(frontpage)
