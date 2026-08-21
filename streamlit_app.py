d8926b3397f11eec317315efa9a826585fd4de21
Luca Braglia <lbraglia@gmail.com>
2026-08-21 15:08:34 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from pathlib import Path
 # ----------
 template_dir = Path("templates")
 commond_dir = Path("common")
-frontpage_file = Path('frontpage.md')
+frontpage_file = Path("frontpage.md")
 common = {
     "TODAY": dt.date.today().isoformat(),
     "PROTOCOL_VERSION": 1,
@@ -34,17 +34,17 @@ st.sidebar.text_input("**Study Acronym**", key="ACRONIMO_STUDIO")
 acronimo_studio = st.session_state.ACRONIMO_STUDIO
 
 templates = {
-    "Protocol | SPIRIT 2025": "protocol_spirit2025.md",
-    "Protocol | STROBE 2007": "protocol_strobe2007.md",
-    "Protocol | STARD 2015": "protocol_stard2015.md",
-    "Protocol | CTSU (rev)": "protocol_ctsu_rev.md",
-    "Protocol | CTSU EN (rev)": "protocol_ctsu_rev_en.md",
-    "Protocol | OLD CTSU": "protocol_ctsu_old.md",
-    "Article | STROBE 2007": "article_strobe2007.md",
-    "Article | STARD 2015": "article_stard2015.md",
-    "Article | CHEERS 2022": "article_cheers2022.md",
-    "Other | SAP": "other_sap.md",
-    "Other | HEAP": "other_heap.md",
+    "Sperimentali | SPIRIT 2025": "protocol_spirit2025.md",
+    # "Protocol | STROBE 2007": "protocol_strobe2007.md",
+    # "Protocol | STARD 2015": "protocol_stard2015.md",
+    "Osservazionali | CTSU (rev)": "protocol_ctsu_rev.md",
+    "Osservazionali (EN) | CTSU EN (rev)": "protocol_ctsu_rev_en.md",
+    # "Protocol | OLD CTSU": "protocol_ctsu_old.md",
+    # "Article | STROBE 2007": "article_strobe2007.md",
+    # "Article | STARD 2015": "article_stard2015.md",
+    # "Article | CHEERS 2022": "article_cheers2022.md",
+    "Altro | SAP": "other_sap.md",
+    "Altro | HEAP": "other_heap.md",
 }
 template_fname = sidebar_selbox_value("**Template**", templates)
 template_path = template_dir / template_fname
@@ -61,17 +61,14 @@ template_path = template_dir / template_fname
 #     "Prospettico": "prospettico",
 # }
 # retroprosp_sel = sidebar_selbox_value("**Retrospettivo/Prospettico**",
-                                      # retroprosp)
+# retroprosp)
 
-formato = {
-    "MS Word": "docx",
-    "Markdown": "md"
-}
+formato = {"MS Word": "docx", "Markdown": "md"}
 formato_sel = sidebar_selbox_value("**File format**", formato)
 
 user_input = {
     "TITOLO_STUDIO": titolo_studio,
-    "ACRONIMO_STUDIO": acronimo_studio  # ,
+    "ACRONIMO_STUDIO": acronimo_studio,  # ,
     # "MONOMULTI": monomulti_sel,
     # "RETROPROSPETTICO": retroprosp_sel,
 }
@@ -81,7 +78,7 @@ user_input = {
 # MAIN PAGE
 # ----------
 # Include README
-with open(frontpage_file, 'r') as fp:
+with open(frontpage_file, "r") as fp:
     frontpage = fp.read()
 
 st.markdown(frontpage)
@@ -105,7 +102,7 @@ with open(template_path, "r") as f:
     template_content = f.read()
 
 # 2) Adding intro if needed
-# 
+#
 # - On one hand protocols have a common intro (administrative info etc) that
 #   are imported
 # - On the other article are taken vanilla, without prefixing intros
@@ -117,7 +114,8 @@ if its_a_protocol:
         "signature_page.md",
         "abbreviations.md",
         # https://github.com/jgm/pandoc/discussions/10609#discussioncomment-13461868
-        "toc.md"]
+        "toc.md",
+    ]
     protocol_intro = []
     for intropart in protocols_intros_templates:
         with open(commond_dir / intropart, "r") as f:
@@ -144,9 +142,11 @@ with open(outfile_md, "w") as f:
 # pandoc = f"pandoc {tmpfname} --lua-filter filters/pagebreak.lua --toc -f markdown -t docx -o {outfile}"
 
 if chosen_format == "docx":
-    pandoc = f"pandoc {outfile_md} --lua-filter filters/pagebreak.lua" \
-        f" --lua-filter=filters/toc-inject.lua" \
+    pandoc = (
+        f"pandoc {outfile_md} --lua-filter filters/pagebreak.lua"
+        f" --lua-filter=filters/toc-inject.lua"
         f" -f markdown -t docx -o {outfile_docx}"
+    )
     subprocess.run(pandoc.split(" "))
 
 
@@ -159,7 +159,4 @@ else:
     raise Warning("Formato file non contemplato.")
 
 with open(outfile, "rb") as f:
-    btn = st.sidebar.download_button(
-        label="Download",
-        data=f,
-        file_name=outfile.name)
+    btn = st.sidebar.download_button(label="Download", data=f, file_name=outfile.name)
